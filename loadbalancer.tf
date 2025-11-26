2f950696b7b7fdf5287cf44a9b5f9def6abd7604
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-26 14:50:05 +0100

    added loadbalancer, targetgroup + listener

---
+++ loadbalancer.tf
@@ -0,0 +1,69 @@
+########################################
+# Application Load Balancer
+########################################
+
+resource "aws_lb" "jwalb" {
+  name               = "jwalb"
+  load_balancer_type = "application"
+  internal           = false. # internet-facing
+
+  # ALB lives in the public subnets
+  subnets = [
+    aws_subnet.jwpublic_1.id,
+    aws_subnet.jwpublic_2.id
+  ]
+
+  security_groups = [
+    aws_security_group.jwsg_alb.id
+  ]
+
+  tags = {
+    Name = "jwalb"
+  }
+}
+
+########################################
+# Target Group (for WordPress EC2 ASG)
+########################################
+
+resource "aws_lb_target_group" "jwalb_tg" {
+  name        = "jwalb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_vpc.jwvpc.id
+
+  # Launchtemplate creates this path /var/www/html/health
+  health_check {
+    protocol            = "HTTP"
+    path                = "/health"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "jwalb-tg"
+  }
+}
+
+########################################
+# Listener (Port 80 -> Target Group)
+########################################
+
+resource "aws_lb_listener" "jwalb_listener" {
+  load_balancer_arn = aws_lb.jwalb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.jwalb_tg.arn
+  }
+
+  tags = {
+    Name = "jwalb-listener"
+  }
+}
