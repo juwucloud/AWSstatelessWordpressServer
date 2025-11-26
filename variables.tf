956f33bdc5a6957df6c978ba3729a5ec0270f9ab
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-26 12:04:16 +0100

    added the most important variables for now

--- variables.tf
+++ variables.tf
@@ -1,5 +1,42 @@
 variable "region" {
   description = "The AWS region for the Project"
   type        = string
-  default     = "us-west-2"
+}
+
+variable "db_username" {
+  type        = string
+  description = "Master username for RDS"
+}
+
+variable "db_password" {
+  type        = string
+  description = "Master password for RDS"
+  sensitive   = true
+}
+
+variable "db_name" {
+  type        = string
+  description = "Initial database name for WordPress"
+  default     = "wordpress"
+}
+
+variable "key_name" {
+  type        = string
+  description = "EC2 Key Pair for optional SSH access"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet CIDR ranges"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet CIDR ranges"
 }
