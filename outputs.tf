cd7bd4454d900b217f045ba41d035de9de1deb0c
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-26 12:13:29 +0100

    commented output out for testing

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
-output "alb_dns_name" {
-  description = "DNS name of the ALB"
-  value       = aws_lb.wp_alb.dns_name
-}
+# output "alb_dns_name" {
+#   description = "DNS name of the ALB"
+#   value       = aws_lb.wp_alb.dns_name
+# }
 
-output "rds_endpoint" {
-  description = "Endpoint of the RDS instance"
-  value       = aws_db_instance.wp_db.endpoint
-}
+# output "rds_endpoint" {
+#   description = "Endpoint of the RDS instance"
+#   value       = aws_db_instance.wp_db.endpoint
+# }
 
-output "efs_id" {
-  description = "ID of the EFS filesystem"
-  value       = aws_efs_file_system.wp_efs.id
-}
+# output "efs_id" {
+#   description = "ID of the EFS filesystem"
+#   value       = aws_efs_file_system.wp_efs.id
+# }
 
-output "access_point_id" {
-  description = "ID of the EFS access point"
-  value       = aws_efs_access_point.wp_ap.id
-}
+# output "access_point_id" {
+#   description = "ID of the EFS access point"
+#   value       = aws_efs_access_point.wp_ap.id
+# }
 
-output "autoscaling_group_name" {
-  description = "Name of the AutoScaling group"
-  value       = aws_autoscaling_group.wp_asg.name
-}
+# output "autoscaling_group_name" {
+#   description = "Name of the AutoScaling group"
+#   value       = aws_autoscaling_group.wp_asg.name
+# }
 
-output "launch_template_id" {
-  description = "ID of the EC2 Launch Template"
-  value       = aws_launch_template.wp_lt.id
-}
+# output "launch_template_id" {
+#   description = "ID of the EC2 Launch Template"
+#   value       = aws_launch_template.wp_lt.id
+# }
