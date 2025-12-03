3a0f028ae77031f58de531f1243968c12af5f6f8
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-12-03 11:29:09 +0100

    changed launchtemplateUserData that wp-config always gets credentials for rds. if not existing, it will be created first

--- LaunchTemplateUserData.sh
+++ LaunchTemplateUserData.sh
@@ -87,17 +87,14 @@ ln -s /mnt/efs /var/www/html/wp-content
 ########################################
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-
     cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-    sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
-    sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
-    sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
-    sed -i "s/localhost/$DB_HOST/" /var/www/html/wp-config.php
-
 fi
 
-
+# Always set DB credentials
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
+sed -i "s/localhost/$DB_HOST/" /var/www/html/wp-config.php
 
 
 ########################################
