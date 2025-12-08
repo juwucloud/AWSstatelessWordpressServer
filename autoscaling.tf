db585f3494255fdc295b8e2bb998bd4e46e03e6a
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-12-08 21:09:38 +0100

    auto scaling group user data added domain_name + now outputs

--- autoscaling.tf
+++ autoscaling.tf
@@ -27,6 +27,7 @@ resource "aws_launch_template" "jwlt" {
     efs_id     = aws_efs_file_system.jwefs.id
     efs_ap_id  = aws_efs_access_point.jwefs_ap.id
     alb_dns    = aws_lb.jwalb.dns_name
+    domain_name = var.domain_name
   }))
 
   tag_specifications {
