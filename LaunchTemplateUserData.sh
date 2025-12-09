28f0971d216ce768a0605dbfaae424553165297b
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-12-09 16:55:09 +0100

    removed referece to hardcoded domain in Userdata

--- LaunchTemplateUserData.sh
+++ LaunchTemplateUserData.sh
@@ -196,9 +196,7 @@ echo "Setting WordPress siteurl & home to $WP_URL and fixing all HTTP references
 mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" <<EOF
 UPDATE wp_options SET option_value='$WP_URL' WHERE option_name='siteurl';
 UPDATE wp_options SET option_value='$WP_URL' WHERE option_name='home';
-UPDATE wp_options SET option_value = REPLACE(option_value, 'http://veganlian.de', 'https://veganlian.de');
-UPDATE wp_posts SET post_content = REPLACE(post_content, 'http://veganlian.de', 'https://veganlian.de');
-UPDATE wp_postmeta SET meta_value = REPLACE(meta_value, 'http://veganlian.de', 'https://veganlian.de');
+
 EOF
 
 ########################################
